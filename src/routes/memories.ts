4010249411488278720ff1d71831acb26353258e
wallace <wallace_wolv@hotmail.com>
2023-05-18 14:54:31 -0300

    feat: CRUD Atualização e remoção de memoria

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -62,8 +62,47 @@ export async function memoriesRoutes(app: FastifyInstance) {
   })
 
   /** Atualização de uma memória */
-  app.put('/memories/:id', async () => {})
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memory.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+      },
+    })
+
+    return memory
+  })
 
   /** Remoção de uma memória */
-  app.delete('/memories/:id', async () => {})
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memory.delete({
+      where: {
+        id,
+      },
+    })
+  })
 }
