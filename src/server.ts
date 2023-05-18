591bd6e768c0ea579b7cb1b4b0e16a1179da51a7
wallace <wallace_wolv@hotmail.com>
2023-05-18 15:03:25 -0300

    feat: Configuração de CORS

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 /* eslint-disable prettier/prettier */
 import fastify from 'fastify';
-
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories';
 
 const app = fastify()
+
+app.register(cors, {
+  // origin: ['http://localhost:3333', 'url de deploy das aplicações frontend']
+  origin: true,
+})
 
 app.register(memoriesRoutes)
 
