e62f9d8f0e0384d911b9996f557b5671663e3e60
wallace <wallace_wolv@hotmail.com>
2023-05-20 22:16:35 -0300

    feat: Rota de upload de arquivos e servindo arquivos estaticos

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,24 @@ import 'dotenv/config'
 
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+/** Essa parte do fastify/static libera para
+ * acessar a imagem dentro do backend, atraves do client */
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   // origin: ['http://localhost:3333', 'url de deploy das aplicações frontend']
@@ -20,12 +32,13 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
